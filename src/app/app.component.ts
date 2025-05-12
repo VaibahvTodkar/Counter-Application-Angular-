101cec03308e8478a9f04cfed0ddab3b420f0300
Vaibhav Todkar <78915720+VaibahvTodkar@users.noreply.github.com>
2025-05-12 13:34:08 +0530

    Initial commit of Counter App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'counterApp';
+  count: number = 0;
+
+  handleCounter(parameter: string) {
+    switch (parameter) {
+      case 'plus':
+        this.count += 1;
+        break;
+      case 'minus':
+        if (this.count > 0) {
+          this.count -= 1;
+        }
+        break;
+      case 'reset':
+      default:
+        this.count = 0;
+        break;
+    }
+  }
 }
